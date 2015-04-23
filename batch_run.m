45dc501de03c810786c370fc0d0a4568876dff22
Jogool <joseph.szurley@esat.kuleuven.be>
2015-04-23 10:26:29 +0200

    sp

--- batch_run.m
+++ batch_run.m
@@ -71,7 +71,7 @@ node_update = 1;
 cost_sum_DANSE = [];
 ii = 1;
 tot_diff = inf;
-% check if we have met either conidtion
+% check if we have met either condition
 while ~or(lt(tot_diff,thresh),ge(ii,max_iter));
     [node] = DANSE(node,node_update);
     cost_sum_DANSE = [cost_sum_DANSE sum(cat(1,node.cost))];
@@ -94,7 +94,7 @@ node_update = updateorder(1);
 cost_sum_TDANSE = [];
 ii = 1;
 tot_diff = inf;
-% check if we have met either conidtion
+% check if we have met either condition
 while ~or(lt(tot_diff,thresh),ge(ii,max_iter));
     [node] = TDANSE(node,node_update);
     cost_sum_TDANSE = [cost_sum_TDANSE sum(cat(1,node.cost))];
@@ -124,7 +124,7 @@ cost_sum_TIDANSE_fc = [];
 node_update = 1;
 ii = 1;
 tot_diff = inf;
-% check if we have met either conidtion
+% check if we have met either condition
 while ~or(lt(tot_diff,thresh),ge(ii,max_iter));
     [node] = TIDANSE_fc(node,node_update);
     cost_sum_TIDANSE_fc = [cost_sum_TIDANSE_fc sum(cat(1,node.cost))];
@@ -153,7 +153,7 @@ node_update = 1;
 cost_sum_TIDANSE_tree = [];
 ii = 1;
 tot_diff = inf;
-% check if we have met either conidtion
+% check if we have met either condition
 while ~or(lt(tot_diff,thresh),ge(ii,max_iter));
     [node] = TIDANSE_tree(node,node_update);
     cost_sum_TIDANSE_tree = [cost_sum_TIDANSE_tree sum(cat(1,node.cost))];
